e10a7f2e850adbb0a0adf852f3df803af7e07c76
András Veres-Szentkirályi <vsza@vsza.hu>
2014-02-08 19:56:42 +0100

    disallow booking already booked gifts

--- wedsite.py
+++ wedsite.py
@@ -42,7 +42,7 @@ def wishlist():
                 else:
                     msg = u'Sikeresen lefoglaltad a kiválasztott ajándéko{0}t, köszönjük!'.format(
                             '' if len(checked) == 1 else 'ka')
-                    cur.executemany('UPDATE wishlist SET booked = ? WHERE id = ?',
+                    cur.executemany('UPDATE wishlist SET booked = ? WHERE id = ? AND booked IS NULL',
                             ((b64encode(name.encode('utf-8')), item) for item in checked))
                     success = True
             items = cur.execute('SELECT id, label, IFNULL(desc, ""), link, booked FROM wishlist').fetchall()
